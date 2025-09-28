4e2541d2b1c63bfe0ab40ba0ac83e7de5e63b060
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-28 19:03:54 -0300

    add: definindo url base e adicionando router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express, { Application, Request, Response } from 'express';
+import { flights } from './data/historyFlights';
+import flightRouter from './routes/flights.route';
 
 const app: Application = express();
 const port: number = 3000;
+const baseUrl: string = '/api/v1';
 
 app.use(express.json());
+app.use(`${baseUrl}/flights`, flightRouter);
 
-app.get('/health', (req: Request, res: Response) => {
-    res.json({ message: 'Olá mundo!' });
+app.get(`${baseUrl}/`, (req: Request, res: Response) => {
+    res.json({ message: 'Servidor rodando...' });
 });
 
 app.listen(port, () =>
-    console.log(`Server running on http://localhost:${port}/health`)
+    console.log(`Server running on http://localhost:${port}${baseUrl}/`)
 );
