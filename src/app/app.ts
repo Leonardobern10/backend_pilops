e83236338f714614072441b22202baff62cc6823
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-30 10:59:50 -0300

    new: instalando cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,25 @@
 import express, { Application, Request, Response } from 'express';
 import flightRouter from '../routes/flights.route.js';
+import cors from 'cors';
 
 // Inicializa uma applicação express
 const app: Application = express();
 // Path padrão obtido de env
 const baseUrl: string = process.env.BASE_URL!;
 
+// Definição do cors para permitir que uma aplicação externa faça a consulta dos dados
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET']
+    })
+);
+
 // Define que o formato utlizado será JSON
 app.use(express.json());
 
 // rota principal para historico de vôos
-app.use(`${baseUrl}/flights`, flightRouter);
+app.use(`${baseUrl}/flight`, flightRouter);
 
 // rota de teste
 app.get(
