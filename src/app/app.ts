f3b47bf63fd1d8a4da08af98e6afed5dac9b4d91
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 11:57:36 -0300

    modified:   src/app/app.ts modified:   src/server.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,27 +6,20 @@ import cors from 'cors';
 const app: Application = express();
 // Path padrão obtido de env
 const baseUrl: string = process.env.BASE_URL!;
+const client: string = process.env.CLIENT_URL || 'http://localhost:5173';
 
 // Definição do cors para permitir que uma aplicação externa faça a consulta dos dados
-app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET']
-    })
-);
+app.use(cors({ origin: client, methods: ['GET'] }));
 
 // Define que o formato utlizado será JSON
 app.use(express.json());
 
 // rota principal para historico de vôos
-app.use(`${baseUrl}/flights`, flightRouter);
+app.use(`/flights`, flightRouter);
 
 // rota de teste
-app.get(
-    `${baseUrl}/`,
-    async (req: Request, res: Response): Promise<Response> => {
-        return res.status(200).json({ message: 'Servidor online...' });
-    }
-);
+app.get(`/`, async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).json({ message: 'Servidor online...' });
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import 'dotenv/config';
 import app from './app/app.js';
 
 // Numero de porta obtido de env
-const port: string = process.env.PORT!;
+const port: string = process.env.PORT || '3000';
 
 // Path padrão obtido de env
 const baseUrl: string = process.env.BASE_URL!;
 
 // Inicia o servidor na porta informada
 app.listen(port, async () =>
-    console.log(`Server running on http://localhost:${port}${baseUrl}/flights`)
+    console.log(`Server running on http://localhost:${port}/flights`)
 );
